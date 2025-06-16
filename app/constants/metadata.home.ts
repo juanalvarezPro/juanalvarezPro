50a107124f33cbac1de3c0b51e82a3c32aacf271
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-06-16 17:46:33 -0500

    feat: add site metadata for portfolio

---
+++ app/constants/metadata.home.ts
@@ -0,0 +1,36 @@
+import { Metadata } from "next";
+
+export const siteMetadata: Metadata = {
+  title: "Juan Alvarez | Full Stack & Embedded Systems Developer",
+  description:
+    "Portfolio web de Juan Camilo Alvarez, desarrollador full stack y especialista en sistemas embebidos (arduino, raspberry, esp32). Descubre proyectos, experiencia y habilidades en desarrollo web y sistemas integrados.",
+  keywords: [
+    "Juan Camilo Alvarez",
+    "medellin arduino",
+    "ESP32",
+    "medellin developer",
+    "Full Stack Developer",
+    "Sistemas Embebidos",
+    "Embedded Systems",
+    "Desarrollador Web",
+    "Portfolio",
+  ],
+  authors: [{ name: "Juan Camilo Alvarez" }],
+  creator: "Juan Camilo Alvarez",
+  openGraph: {
+    title: "Juan Camilo Alvarez | Full Stack & Embedded Systems Developer",
+    description:
+      "Portfolio web de Juan Camilo Alvarez, desarrollador full stack y especialista en sistemas embebidos.",
+    url: "https://juanalvarez.pro", // Cambia por tu dominio real
+    siteName: "Soy Juan Alvarez",
+    type: "website",
+    locale: "es_CO",
+  },
+//   twitter: {
+//     card: "summary_large_image",
+//     title: "Juan Camilo Alvarez | Full Stack & Embedded Systems Developer",
+//     description:
+//       "Portfolio web de Juan Camilo Alvarez, desarrollador full stack y especialista en sistemas embebidos.",
+//     creator: "@tu_usuario", // Cambia por tu usuario real de Twitter si tienes
+//   },
+};
