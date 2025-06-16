11f79cb2cfec68b97358290e74ae13b4f98b9349
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-06-16 18:15:03 -0500

    refactor: simplify NavMenu by using a dynamic menuItems array

--- components/navbar/nav-menu.tsx
+++ components/navbar/nav-menu.tsx
@@ -8,27 +8,25 @@ import { cn } from "@/lib/utils";
 import { NavigationMenuProps } from "@radix-ui/react-navigation-menu";
 import Link from "next/link";
 
+const menuItems = [
+  { href: "#experience", label: "Experiencia" },
+  { href: "#projects", label: "Proyectos" },
+  { href: "#about", label: "Sobre Mi" },
+];
+
 export const NavMenu = ({ className, ...props }: NavigationMenuProps) => (
   <NavigationMenu
     className={cn("data-[orientation=vertical]:items-start", className)}
     {...props}
   >
     <NavigationMenuList className="gap-1 space-x-0 data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start">
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild>
-          <Link href="#about">About</Link>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild>
-          <Link href="#experience">Experience</Link>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuLink asChild>
-          <Link href="#projects">Projects</Link>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
+      {menuItems.map(({ href, label }) => (
+        <NavigationMenuItem key={href}>
+          <NavigationMenuLink asChild>
+            <Link href={href}>{label}</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      ))}
     </NavigationMenuList>
   </NavigationMenu>
 );
