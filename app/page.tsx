28e3c85ce0dac1de07168df7429bafc9da64a429
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-06-16 17:38:45 -0500

    fix: reorder components in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <div className="space-y-10 sm:space-y-16">
       <Hero />
-      <About />
       <Experience />
       <Projects />
+      <About />
     </div>
   );
 }
