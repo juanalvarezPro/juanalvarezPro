9091bf11577c015027b6f0486c6e8fd252ba961b
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-06-16 19:48:10 -0500

    refactor: update Logo component styles and replace icon with text

--- components/navbar/logo.tsx
+++ components/navbar/logo.tsx
@@ -1,8 +1,6 @@
-import { SquareTerminal  } from "lucide-react";
 export const Logo = () => (
-  <div className="flex items-center space-x-0.5 font-bold text-lg">
-    <SquareTerminal/>
+  <div className="flex items-center space-x-0.5 font-bold text-basic">
     <span className="text-bold">JuanAlvarez.</span>
-    <span className="text-black/50">pro</span>
+    <span className="text-muted-foreground">Pro</span>
   </div>
 );
