2b681646032230e0823cfda067e66e72c4242784
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-06-17 00:42:48 -0500

    feat: remove unused logo imports from Footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
-import { BlueSkyLogo, DribbleLogo, GithubLogo, XLogo } from "./icons";
+import {GithubLogo} from "./icons";
 
 const footerLinks = [
   {
