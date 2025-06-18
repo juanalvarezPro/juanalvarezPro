752dd484a819080cd10bb0b8fe4587a8e88e517d
JuanAlvarezzzDev <juanalvarezzz.dev@gmail.com>
2025-06-18 01:21:24 -0500

    feat: add displayName prop to ButtonGithub for conditional rendering

--- components/Buttons/ButtonGithub.tsx
+++ components/Buttons/ButtonGithub.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { GithubLogo } from "../icons";
-
-const ButtonGithub = () => {
+interface ButtonGithubProps {
+  displayName?: boolean;
+}
+const ButtonGithub = ({ displayName }: ButtonGithubProps) => {
   return (
-    <Button asChild variant="outline" className="rounded-full shadow-none" size="icon">
+    <Button asChild variant="outline" className="rounded-full shadow-none" size={`${displayName ? "icon" : "default"}`}>
       <Link href="https://github.com/JuanAlvarezzzDev" target="_blank">
         <GithubLogo className="h-5 w-5" />
+        <span className={`${displayName ? "hidden" : "block"}`}>Ver Github</span>
       </Link>
     </Button>
   );
