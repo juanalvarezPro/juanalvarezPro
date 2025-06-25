981056fec45efde9c99735e44fcc955d70b8e71a
juanalvarezPro <juanalvarezzz.dev@gmail.com>
2025-06-25 00:46:45 -0500

    refactor: update ButtonGithub component to use ButtonProps for improved type safety

--- src/components/Buttons/ButtonGithub.tsx
+++ src/components/Buttons/ButtonGithub.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { GithubLogo } from "../icons";
-interface ButtonGithubProps {
-  displayName?: boolean;
-}
-const ButtonGithub = ({ displayName }: ButtonGithubProps) => {
+import { ButtonProps } from "@/src/types/ButtonProps";
+
+const ButtonGithub = ({ displayName }: ButtonProps) => {
   return (
     <Button asChild variant="outline" className="rounded-full shadow-none" size={`${displayName ? "icon" : "default"}`}>
       <Link href="https://github.com/juanalvarezPro" target="_blank">
