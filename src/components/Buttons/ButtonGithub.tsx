596b8584d7e7071eb5e9c6a7f01aeb90252ad60b
juanalvarezPro <juanalvarezzz.dev@gmail.com>
2025-06-28 12:20:05 -0500

    feat: refactor ButtonGithub component for improved readability and dynamic GitHub link

--- src/components/Buttons/ButtonGithub.tsx
+++ src/components/Buttons/ButtonGithub.tsx
@@ -3,13 +3,24 @@ import Link from "next/link";
 import { Button } from "../ui/button";
 import { GithubLogo } from "../icons";
 import { ButtonProps } from "@/src/types/ButtonProps";
+import { myContact } from "@/src/app/constants/mycontact";
 
 const ButtonGithub = ({ displayName }: ButtonProps) => {
   return (
-    <Button asChild variant="outline" className="rounded-full shadow-none" size={`${displayName ? "icon" : "default"}`}>
-      <Link href="https://github.com/juanalvarezPro" target="_blank">
+    <Button
+      asChild
+      variant="outline"
+      className="rounded-full shadow-none"
+      size={`${displayName ? "icon" : "default"}`}
+    >
+      <Link
+        href={`https://github.com/${myContact.githubAccount}`}
+        target="_blank"
+      >
         <GithubLogo className="h-5 w-5" />
-        <span className={`${displayName ? "hidden" : "block"}`}>Ver Github</span>
+        <span className={`${displayName ? "hidden" : "block"}`}>
+          Ver Github
+        </span>
       </Link>
     </Button>
   );
