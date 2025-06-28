d465cbe93298ab8fe7543019a90eb3e49e8330ea
juanalvarezPro <juanalvarezzz.dev@gmail.com>
2025-06-28 12:19:12 -0500

    feat: add neueMachina custom font for enhanced typography

---
+++ src/fonts.ts
@@ -0,0 +1,18 @@
+import { Geist } from "next/font/google";
+import localFont from "next/font/local";
+
+// Google Font
+export const geistSans = Geist({
+  subsets: ["latin"],
+});
+
+// fuente personalizada
+export const neueMachina = localFont({
+  src: [
+    {
+      path: "../public/fonts/MyFont.woff2", 
+      weight: "400",
+    },
+  ],
+  display: "swap",
+});
